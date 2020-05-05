b9a90e6eb35b585383ad5f9e44eb169f28e24635
sachiantany <52045272+sachiantany@users.noreply.github.com>
2020-05-06 03:18:17 +0530

    Update sensorService.js

--- src/service/sensorService.js
+++ src/service/sensorService.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export class sensorService{
     baseUrl = "http://localhost:8080/firealarm/"
 
+/* Read API data from axios*/
     getAll(){
         return axios.get(this.baseUrl + "sensors")
             .then(res => res.data);
